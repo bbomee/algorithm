1f1a84b8f7f24251fd43070e0e697f1a4e465a0e
Boyeon Jeong <bbomee16@gmail.com>
2021-01-14 16:33:29 +0900

    Length of Last Word
    주어진 String의 마지막 단어의 길이 return

---
+++ src/leetCode/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+public class LengthOfLastWord {
+
+	public static void main(String[] args) {
+		String s = "Hello World";
+		System.out.println(lengthOfLastWord(s));
+	}
+    public static int lengthOfLastWord(String s) {
+        String[] sArr = s.split(" ");
+    	if(sArr.length == 0) {
+    		return 0;
+    	}
+        return sArr[sArr.length-1].length();
+    }
+}
