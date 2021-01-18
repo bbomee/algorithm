6553f9edeaa7cb342fcf531e5a40677224fe08b3
Boyeon Jeong <bbomee16@gmail.com>
2021-01-18 10:38:26 +0900

    주어진 배열중  연속한배열의 합들 중 가장 큰 값 return

---
+++ src/leetCode/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] input = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(input));
+	}
+    public static int maxSubArray(int[] nums) {
+    	int maxSum = nums[0];
+    	for(int i = 0; i < nums.length; i++) {
+    		int maxTemp = nums[i];
+    		int temp = 0;
+    		for(int j = i; j < nums.length; j++) {
+    			temp = temp + nums[j];
+    			if(maxTemp <= temp) {
+    				maxTemp = temp;
+    			}
+    		}
+    		if(maxSum <= maxTemp) {
+    			maxSum = maxTemp;
+    		}
+    	}
+    	return maxSum;
+    }
+
+}
