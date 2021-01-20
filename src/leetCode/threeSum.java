2b7f37d25c639d156eac9e8eea31bf4d597f9ba4
Boyeon Jeong <bbomee16@gmail.com>
2021-01-20 15:50:45 +0900

    threeSum 더 빠르게

--- src/leetCode/threeSum.java
+++ src/leetCode/threeSum.java
@@ -19,31 +19,36 @@ public class threeSum {
 	}
 	public static  List<List<Integer>> threeSum(int[] nums) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
-    	Arrays.parallelSort(nums);
+		Arrays.parallelSort(nums);
 
-    	for(int i = 0; i < nums.length-2; i++) {
-    		if(nums[i] > 0) break;
-    		if(i > 0 && nums[i] == nums[i-1] ) {
-    			continue;
-    		}
-    		for(int j = i+1; j < nums.length-1; j++) {
-    			if(nums[i] + nums[j] > 0 ) break;
-    				if(j >= i+2 && nums[j] == nums[j-1]) {
-    						continue;
-    				}
-    			for(int k = j+1; k < nums.length; k++) {
-    				if(nums[i] + nums[j] + nums[k] == 0) {
-    					List<Integer> three = new ArrayList<Integer>();
-    					three.add(nums[i]);
-    					three.add(nums[j]);
-    					three.add(nums[k]);
-    					result.add(three);
-    					break;
-    				} 
-    			}
-    		}
-    	}
-    	return result; 
+		for(int i = 0; i < nums.length-2; i++) {
+			if(nums[i] > 0) break;
+			if(i > 0 && nums[i] == nums[i-1] ) continue;
+
+			int left = i+1;
+			int right = nums.length-1;
+
+			while(left < right) {
+				int sum = nums[i] + nums[left] + nums[right];
+				if(sum == 0) {
+					List<Integer> tl = new ArrayList<>();
+					tl.add(nums[i]);
+					tl.add(nums[left]);
+					tl.add(nums[right]);
+					result.add(tl);
+					left++;
+					right--;
+					while(nums[left] == nums[left-1] && left < right) left++;
+					while(nums[right] == nums[right+1] && left < right) right--;
+				} else if(sum < 0) {
+					left++;
+				} else if(sum > 0) {
+					right--;
+				}
+			}
+
+		}
+		return result; 
 	}
 
 }
